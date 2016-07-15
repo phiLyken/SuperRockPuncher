08a1978a849235d9f5756f57cb27c77ca7e4b0ff
phil <philipp.zupke@wooga.net>
2016-07-15 12:24:12 +0200

    Added punchmeter

---
+++ Assets/PunchMeter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchMeter : MonoBehaviour {
+
+	public float CurrentFill;
+
+	public float FillRate;
+
+	public float PunchCost;
+
+	public AnimationCurve FillToRange;
+
+	public float Range;
+
+
+	void Update(){
+		CurrentFill = Mathf.Clamp01( CurrentFill + Time.deltaTime * FillRate);
+
+
+	}
+	public void Punch(){
+		CurrentFill-=PunchCost;
+	}
+
+	public float GetRange(){
+		return FillToRange.Evaluate( CurrentFill) * Range;
+	}
+
+
+}
