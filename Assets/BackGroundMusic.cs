e74430284f109600bf7909ac697c3064c035ecb2
phil <philipp.zupke@wooga.net>
2016-08-01 12:45:00 +0200

    added background music and music toggle button in main menu

---
+++ Assets/BackGroundMusic.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundMusic : MonoBehaviour {
+
+	public AudioSource bg_music;
+	static BackGroundMusic instance;
+
+	static string _music_setting_key ="_music_enabled";
+
+
+	public static bool GetEnabled(){
+		
+		return PlayerPrefs.GetInt(_music_setting_key) == 1;
+	}
+
+	void Awake(){
+		
+		if(!PlayerPrefs.HasKey(_music_setting_key)){
+			PlayerPrefs.SetInt(_music_setting_key,1);
+		}
+
+		if(instance != null){
+			Destroy(this);
+
+		} else {
+			instance = this;
+			DontDestroyOnLoad(this);
+		}
+
+		instance.SetBGMusic();
+
+
+	}
+
+	public static void ToggleMusic(){
+
+		PlayerPrefs.SetInt(_music_setting_key, GetEnabled() ? 0 : 1);
+
+		instance.SetBGMusic();
+		
+	}
+	void SetBGMusic(){
+		
+		if(GetEnabled()){
+			if(bg_music.isPlaying){
+				//bg_music.Stop();
+			}
+			bg_music.Play();
+			Debug.Log("Start music");
+		} else {
+			bg_music.Stop();
+		}
+	}
+
+
+}
---
+++ Assets/ToggleBackgroundButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ToggleBackgroundButton : MonoBehaviour {
+
+	public Sprite Enabled_Image;
+	public Sprite Disabled_Image; 
+
+	public Image Image;
+
+	public void Toggle(){
+		BackGroundMusic.ToggleMusic();
+		SetImage();
+	}
+
+	void SetImage(){
+		Image.sprite = BackGroundMusic.GetEnabled() ? Enabled_Image : Disabled_Image;
+	}
+
+	void Start(){
+		SetImage();
+	}
+}
